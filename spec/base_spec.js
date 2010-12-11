c319a67ef2d3c5e1ee68291bb2a47af204879aa5
Ryan Greenberg <ryangreenberg@gmail.com>
2010-12-10 18:43:29 -0800

    adds .methods() to fecha.util

--- spec/base_spec.js
+++ spec/base_spec.js
@@ -1,3 +1,23 @@
-describe("fecha", function() {
+describe("fecha", function(){
+
+  describe("util", function(){
+
+    describe("#methods", function(){
+      it("copies functions to the prototype of an object", function(){
+        var TestObject = function(){},
+        obj = new TestObject();
+
+        fecha.util.methods(TestObject, {
+          aMethod: function(){ return true; },
+          bMethod: function(){ return this; }
+        });
+
+        expect(typeof obj.aMethod).toEqual('function');
+        expect(obj.aMethod()).toEqual(true);
+        expect(obj.bMethod()).toEqual(obj);
+      });
+    });
+
+  });
 
 });
--- src/base.js
+++ src/base.js
@@ -1,5 +1,11 @@
 var fecha = {};
 
 (function(F){
-    
+    F.util = {
+        methods: function(obj, fns) {
+            for (var fn in fns) {
+                obj.prototype[fn] = fns[fn];
+            }
+        }
+    };
 })(fecha);
