f50d5067775673803f2ba9ee5dd6ce9a9274bc83
Ryan Greenberg <ryangreenberg@gmail.com>
2010-12-12 13:25:30 -0800

    adds fecha.add method for relative date offsets

--- src/date.js
+++ src/date.js
@@ -1,5 +1,5 @@
 (function(F){
-    
+
     // Private functions
     // Pad a number with leading zeros until it is digits long
     var zeroPad = function(n, digits) {
@@ -9,50 +9,68 @@
         }
         return n;
     };
-    
-    
+
+
     // Public methods
     F.clone = function(date){
         return new Date(date.getTime());
     };
-    
-    
+
+
     F.sameDate = function(date1, date2){
         return date1.getFullYear() == date2.getFullYear() &&
                date1.getMonth() == date2.getMonth() &&
                date1.getDate() == date2.getDate();
     };
     // prototype name: sameAs, sameDateAs
-    
-    
+
+
     // Set multiple properties of a date object at once
     // For example:
     //   var d = new Date();
-    //   d.set{hours: 8, minutes: 30, seconds: 0};
+    //   fecha.set(d, {hours: 8, minutes: 30, seconds: 0});
     F.set = function(date, obj) {
         for (var key in obj) {
-            var method = key.charAt(0).toUpperCase() + key.slice(1); // titlecase
-            if (date['set' + method]) {
-                date['set' + method](obj[key]);
+            var methodName = key.charAt(0).toUpperCase() + key.slice(1), // titlecase
+                setter = 'set' + methodName;
+
+            if (date[setter]) {
+                date[setter](obj[key]);
             }
         }
         return date;
     };
-    
-    
+
+    // Add relative values to properties of a date object at once
+    // For example:
+    //  var d = new date();
+    //  fecha.add(d, {hours: -1, minutes: -30});
+    F.add = function(date, obj) {
+        for (var key in obj) {
+            var methodName = key.charAt(0).toUpperCase() + key.slice(1), // titlecase
+                setter = 'set' + methodName,
+                getter = 'get' + methodName;
+
+            if (date[setter]) {
+                date[setter](date[getter]() + (obj[key] * 1));
+            }
+        }
+        return date;
+    };
+
     // Given a date returns it formatted as YYYY-MM-DD
     F.isoDate = function(date) {
         return date.getFullYear() + '-' +
             zeroPad(date.getMonth() + 1, 2) +
             '-' + zeroPad(date.getDate(), 2);
     };
-    
-    
+
+
     // Returns the time component of a date formatted as HH:MM:SS
     F.hms = function(date) {
         return zeroPad(date.getHours(), 2) + ':' +
             zeroPad(date.getMinutes(), 2) + ':' +
             zeroPad(date.getSeconds(), 2);
     };
-    
+
 })(fecha);
